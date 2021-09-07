b2e12dd610a1e928ca2af3b07e68d85523ce3d79
jinwook-song <wlsdnr129@naver.com>
2021-09-07 23:17:45 +0900

    JS Event

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 const title = document.getElementById("title");
 
 title.innerHTML = "got you";
+
+function handleTitleMousedown() {
+  title.style.color = "blue";
+}
+function handleTitleMouseUp() {
+  title.style.color = "red";
+}
+function handleTitleClick() {
+  title.classList.toggle("clicked");
+}
+
+function handleWindowCopy() {
+  alert("copied!");
+}
+
+// title.addEventListener("mouseenter", handleTitleMousedown);
+// title.addEventListener("mouseleave", handleTitleMouseUp);
+title.addEventListener("click", handleTitleClick);
+
+window.addEventListener("copy", handleWindowCopy);
